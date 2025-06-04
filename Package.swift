a5cf74ff42bd4b7442aae646a1a87b7a4e1f742b
nya <home@cathand.org>
2025-06-04 14:33:00 +0900

    package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GLDTween",
+    products: [
+        .library(
+            name: "GLDTween",
+            targets: ["GLDTween"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "GLDTween",
+            dependencies: [],
+            path: "GLDTween")
+    ]
+)
